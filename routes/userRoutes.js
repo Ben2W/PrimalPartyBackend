b9daeda2105648e9ae71015695c5a3f737211589
Benjamin W <bewerner23@gmail.com>
2022-03-18 19:12:49 -0400

    Added register route.
    added register route.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const userRouter = require('express').Router();
 const passport = require('passport');
-const passwordUtils = require('../lib/passwordUtils');
+const genPassword = require('../lib/passwordUtils').genPassword;
 const connection = require('../config/database');
 const User = connection.models.User;
 
@@ -17,9 +17,24 @@ const User = connection.models.User;
 
  // TODO
  userRouter.post('/register', (req, res, next) => {
+    
+    const saltHash = genPassword(req.body.password);
 
+    const salt = saltHash.salt;
+    const hash = saltHash.hash;
 
+    const newUser = new User({
+        username: req.body.uname,
+        hash: hash, 
+        salt: salt
+    })
 
+    newUser.save()
+        .then((user) => {
+            console.log(user)
+        });
+
+    res.redirect('/login')
  });
 
 
