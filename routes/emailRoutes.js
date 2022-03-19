74c6baed79950eab57ffee6dc6b1d0f6961bafc6
Benjamin W <bewerner23@gmail.com>
2022-03-19 19:59:00 -0400

    Added put /forgot route.
    This route currently generates a token, attaches the token to the user, then sends them an email with the token.

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -10,7 +10,21 @@ const express = require('express')
 emailRouter.use(express.urlencoded({extended:true}))
 emailRouter.use(express.json())
 
+// Sets up sendgrid dependancies.
+const sgMail = require('@sendgrid/mail')
+require('dotenv').config()
+const sgMAILAPI = process.env.SENDGRID_API_KEY
+sgMail.setApiKey(sgMAILAPI)
 
+
+
+
+
+
+/*
+* Generates a token the user will receive via email if they request a password change.
+*
+*/
 function generateToken() {
     var buf = new Buffer.alloc(16);
     for (var i = 0; i < buf.length; i++) {
@@ -33,21 +47,23 @@ emailRouter.put('/forgot', catchAsync(async(req, res, next) => {
     //@TODO: in the User Schema, keep track of when a resetToken was generated. To limit users from reseting passwords. IE: If a token was generated 10 seconds ago, deny this forgot password request.
     const user = await User.findOneAndUpdate({email: email, resetToken: token});
     console.log(user);
-    res.json({token: token});
+
+    const message = {
+        to: 'bewerner23@gmail.com',
+        from: 'no-reply@primaljet.com',
+        templateId: 'd-e4e8ed898ae346a888e742ca2b954234',
+        dynamicTemplateData: {
+            name: user.username,
+            link: token,
+        },
+    };
+
+
+
+
+    sgMail.send(message)
+        .then(response => res.json({status: 'email sent'}))
+    .catch(err => res.json({status: 'error: email not sent'}));
 })); 
-
-
-
-// emailRouter.get('/forgot', function (req, res) {
-//     if (req.isAuthenticated()) {
-//         //user is alreay logged in
-//         return res.redirect('/');
-//     }
-
-//     //UI with one input for email
-//     console.log("FORGOT")
-// });
-
-
 
 module.exports = emailRouter;
