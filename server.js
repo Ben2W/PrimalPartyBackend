8bdd363257e125cdcaa4bc317292ddd72c1857ff
Benjamin W <bewerner23@gmail.com>
2022-03-24 20:15:49 -0400

    Fixed credentials issue

--- server.js
+++ server.js
@@ -49,7 +49,12 @@ const { userSchema, eventSchema, taskSchema } = require('./schemas.js');
 // Middleware
 const {isLoggedIn, isAdmin} = require('./middleware') 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://black-ocean-0eed1b40f.1.azurestaticapps.net'],
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}))
+
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
