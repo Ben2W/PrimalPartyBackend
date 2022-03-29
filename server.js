5bcd4755081428f570ed315ce9ccd8c0651f0ec4
Benjamin W <bewerner23@gmail.com>
2022-03-29 04:02:00 -0400

    More experimentation

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.use(session({
         httpOnly: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24, //Equals 1 day
-        sameSite: "none"
+        sameSite: "lax"
     }
 }))
 
