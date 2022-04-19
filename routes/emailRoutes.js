8735af44b5732d2951b44bfd6ab71ef1c522fd17
Benjamin W <bewerner23@gmail.com>
2022-04-19 00:15:15 -0400

    8===D

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -110,7 +110,7 @@ emailRouter.put('/forgot', catchAsync(async(req, res, next) => {
     //const url = 'http://' + req.headers.host.toString() + /reset/ + token.toString()
     const message = {
         to: email,
-        from: 'no-reply@primaljet.com',
+        from: 'no-reply@primalparty.com',
         templateId: 'd-e4e8ed898ae346a888e742ca2b954234',
         dynamicTemplateData: {
             name: user.username,
